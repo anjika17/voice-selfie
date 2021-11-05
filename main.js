e96e20abcc0d6e420ed0bdb9bbe4afd33f93596d
anjika17 <80506928+anjika17@users.noreply.github.com>
2021-11-05 13:50:35 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -8,4 +8,23 @@ function start(){
 recognition.onresult = function(event) {
   console.log(event) ;  
   var Content = event.results[0][0].transcript ;
+  console.log(Content) ;
+  document.getElementById("textbox").innerHTML = Content ;
+  if(Content == "take my selfie"){
+   console.log("Taking a selfie -------------") ;
+   speak() ;
+  }
 }
+function speak(){
+ var synth = window.speechSynthesis ;
+ var speak_data = "Taking your selfie in 5 seconds"  ;
+ var utter_this = new SpeechSynthesisUtterance(speak_data) ;
+ synth.speak(utter_this);
+}
+camera = document.getElementById("camera") ;
+Webcam.set({
+  width:360 ,
+  height:260 ,
+  image_format:'png' ,
+  png_quality:90
+}) ;
