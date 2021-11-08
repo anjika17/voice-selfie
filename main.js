478f014c74d676f80c23d4ed4a5d629d6aa49144
anjika17 <80506928+anjika17@users.noreply.github.com>
2021-11-08 13:41:10 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -20,7 +20,12 @@ function speak(){
  var speak_data = "Taking your selfie in 5 seconds"  ;
  var utter_this = new SpeechSynthesisUtterance(speak_data) ;
  synth.speak(utter_this);
-}
+Webcam.attach(camera);
+setTimeout(function(){
+ take_snapshot();
+ save(); 
+}, 5000) ;
+} 
 camera = document.getElementById("camera") ;
 Webcam.set({
   width:360 ,
@@ -28,3 +33,17 @@ Webcam.set({
   image_format:'png' ,
   png_quality:90
 }) ;
+
+function take_snapshot(){
+  Webcam.snap(function(data_uri)
+  {
+document.getElementById("result").innerHTML = '<img id="selfie" src="'+ data_uri +'">' ;
+  }) ;
+  }
+
+function save(){
+  link = document.getElementById("link") ;
+  image = document.getElementById("selfie").src ;
+  link.href = image ;
+  link.click();
+}
